f54f08c5241457aeed9f9f56a82158f90e8b4a13
Antoine Lever <antoinelever@me.com>
2022-12-05 13:10:25 +0000

    Fix empty Status field

--- lib/src/models/notion.dart
+++ lib/src/models/notion.dart
@@ -12,8 +12,8 @@ class Notion {
   Notion.fromJson(Map<String, dynamic> json, {this.project})
       : id = json["id"],
         task = json["properties"]["Task"]['title'].first['text']['content'],
-        status = json["properties"]["Status"].keys.contains('name')
-            ? json["properties"]["Status"]["name"]
+        status = json["properties"]["Status"]["select"] != null
+            ? json["properties"]["Status"]["select"]["name"]
             : null,
         url = json["url"];
 
